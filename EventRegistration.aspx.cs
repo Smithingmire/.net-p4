87e8b67a0a2ce1c3c11459303e1e9066a5332032
Smithingmire <smithingmire12@gmail.com>
2026-08-16 18:32:55 +0530

    practical 4 first commit

---
+++ EventRegistration.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineEventRegistration
+{
+    public partial class EventRegistration : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                lblMessage.Text = "Registration Successful!";
+                lblMessage.ForeColor = Color.Green;
+            }
+        }
+
+        protected void cvTerms_ServerValidate(
+            object source,
+            ServerValidateEventArgs args)
+        {
+            args.IsValid = chkTerms.Checked;
+        }
+    }
+}
